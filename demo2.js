586482691919b7078ef165007dc0e91a0a73fb82
Syed Maaz Hasan <49359456+SyedMaazHasan@users.noreply.github.com>
2019-11-02 18:19:13 +0530

    Create demo2.js

---
+++ demo2.js
@@ -0,0 +1,47 @@
+const graphql = require('graphql');
+const Book = require('../models/book');
+const Author = require('../models/Author');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                return Book.findById(args.id);
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                return Author.findById(args.id);
+            }
+        }
+    }
+});
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return Book.find({ authorId: parent.id });
+            }
+        }
+    })
+});
